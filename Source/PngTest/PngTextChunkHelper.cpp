4790f9993b9b32779313c31ea58c24696ab94226
Naotsun <51815450+Naotsun19B@users.noreply.github.com>
2021-03-30 16:56:13 +0900

    Commit for the time being

--- Source/PngTest/PngTextChunkHelper.cpp
+++ Source/PngTest/PngTextChunkHelper.cpp
@@ -180,27 +180,56 @@ bool FPngTextChunkHelper::Write(const TMap<FString, FString>& MapToWrite)
 		return false;
 	}
 
-	png_structp WritePtr = png_create_write_struct(
-		PNG_LIBPNG_VER_STRING, 
-		ImageWrapper.Get(), 
+	const ERGBFormat Format = ImageWrapper->GetFormat();
+	const int32 BitDepth = ImageWrapper->GetBitDepth();
+	TArray<uint8> RawData;
+	ImageWrapper->GetRaw(Format, ImageWrapper->GetBitDepth(), RawData);
+	const int32 Height = ImageWrapper->GetHeight();
+	const int32 Width = ImageWrapper->GetWidth();
+	if (RawData.Num() == 0 || Height == 0 || Width == 0)
+	{
+		return false;
+	}
+
+	png_structp png_ptr = png_create_write_struct(
+		PNG_LIBPNG_VER_STRING,
+		ImageWrapper.Get(),
 		PngTextChunkHelperInternal::FLibPngEventHandler::user_error_fn,
 		PngTextChunkHelperInternal::FLibPngEventHandler::user_warning_fn
 	);
-	png_infop InfoPtr = png_create_info_struct(WritePtr);
-	png_bytep* RowPointers = (png_bytep*)png_malloc(WritePtr, ImageWrapper->GetHeight() * sizeof(png_bytep));
+	png_infop info_ptr = png_create_info_struct(png_ptr);
+	png_bytep* row_pointers = (png_bytep*)png_malloc(png_ptr, Height * sizeof(png_bytep));
+	PngTextChunkHelperInternal::FPngWriteGuard PNGGuard(&png_ptr, &info_ptr);
+	PNGGuard.SetRowPointers(row_pointers);
 
-	PngTextChunkHelperInternal::FPngWriteGuard WriteGuard(&WritePtr, &InfoPtr);
-	WriteGuard.SetRowPointers(RowPointers);
-
-	if (WritePtr == nullptr || InfoPtr == nullptr)
+	if(png_ptr == NULL || info_ptr == NULL)
 	{
 		return false;
 	}
 
-	if (setjmp(png_jmpbuf(WritePtr)) != 0)
+	if (setjmp(png_jmpbuf(png_ptr)) != 0)
 	{
 		return false;
 	}
+
+	png_set_compression_level(png_ptr, Z_BEST_SPEED);
+	png_set_IHDR(
+		png_ptr, 
+		info_ptr, 
+		Width, 
+		Height, 
+		BitDepth, 
+		(Format == ERGBFormat::Gray) ? PNG_COLOR_TYPE_GRAY : PNG_COLOR_TYPE_RGBA, 
+		PNG_INTERLACE_NONE, 
+		PNG_COMPRESSION_TYPE_DEFAULT, 
+		PNG_FILTER_TYPE_DEFAULT
+	);
+	png_set_write_fn(
+		png_ptr, 
+		ImageWrapper.Get(),
+		PngTextChunkHelperInternal::FLibPngEventHandler::user_write_compressed,
+		PngTextChunkHelperInternal::FLibPngEventHandler::user_flush_data
+	);
 
 	const int32 NumText = MapToWrite.Num();
 	TArray<png_text> TextPtr;
@@ -221,16 +250,20 @@ bool FPngTextChunkHelper::Write(const TMap<FString, FString>& MapToWrite)
 
 		Index++;
 	}
+	png_set_text(png_ptr, info_ptr, TextPtr.GetData(), NumText);
 
-	png_set_write_fn(
-		WritePtr, 
-		ImageWrapper.Get(), 
-		PngTextChunkHelperInternal::FLibPngEventHandler::user_write_compressed,
-		PngTextChunkHelperInternal::FLibPngEventHandler::user_flush_data
-	);	
-	png_set_text(WritePtr, InfoPtr, TextPtr.GetData(), NumText);
-	uint32 Transforms = (ImageWrapper->GetFormat() == ERGBFormat::BGRA) ? PNG_TRANSFORM_BGR : PNG_TRANSFORM_IDENTITY;
-	png_write_png(WritePtr, InfoPtr, Transforms, NULL);
+	const uint64 PixelChannels = (Format == ERGBFormat::Gray) ? 1 : 4;
+	const uint64 BytesPerPixel = (BitDepth * PixelChannels) / 8;
+	const uint64 BytesPerRow = BytesPerPixel * Width;
+
+	for (int64 i = 0; i < Height; i++)
+	{
+		row_pointers[i] = &RawData[i * BytesPerRow];
+	}
+	png_set_rows(png_ptr, info_ptr, row_pointers);
+
+	uint32 Transform = (Format == ERGBFormat::BGRA) ? PNG_TRANSFORM_BGR : PNG_TRANSFORM_IDENTITY;
+	png_write_png(png_ptr, info_ptr, Transform, NULL);
 
 	return true;
 }
@@ -242,29 +275,125 @@ bool FPngTextChunkHelper::Read(TMap<FString, FString>& MapToRead)
 		return false;
 	}
 
-	png_structp ReadPtr = png_create_read_struct_2(
-		PNG_LIBPNG_VER_STRING,
-		ImageWrapper.Get(),
-		PngTextChunkHelperInternal::FLibPngEventHandler::user_error_fn,
-		PngTextChunkHelperInternal::FLibPngEventHandler::user_warning_fn,
-		NULL,
-		PngTextChunkHelperInternal::FLibPngEventHandler::user_malloc,
-		PngTextChunkHelperInternal::FLibPngEventHandler::user_free
-	);
-	png_infop InfoPtr = png_create_info_struct(ReadPtr);
-
-	PngTextChunkHelperInternal::FPngReadGuard ReadGuard(&ReadPtr, &InfoPtr);
-
-	if (ReadPtr == nullptr || InfoPtr == nullptr)
+	const ERGBFormat Format = ImageWrapper->GetFormat();
+	const int32 BitDepth = ImageWrapper->GetBitDepth();
+	TArray<uint8> RawData;
+	ImageWrapper->GetRaw(Format, ImageWrapper->GetBitDepth(), RawData);
+	const int32 Height = ImageWrapper->GetHeight();
+	const int32 Width = ImageWrapper->GetWidth();
+	if (RawData.Num() == 0 || Height == 0 || Width == 0 ||
+		!(Format == ERGBFormat::BGRA || Format == ERGBFormat::RGBA || Format == ERGBFormat::Gray) ||
+		!(BitDepth == 8 || BitDepth == 16))
 	{
 		return false;
 	}
 
-	png_read_png(ReadPtr, InfoPtr, PNG_TRANSFORM_IDENTITY, NULL);
+	png_structp png_ptr = png_create_read_struct_2(
+		PNG_LIBPNG_VER_STRING, 
+		ImageWrapper.Get(), 
+		PngTextChunkHelperInternal::FLibPngEventHandler::user_error_fn,
+		PngTextChunkHelperInternal::FLibPngEventHandler::user_warning_fn,
+		NULL, 
+		PngTextChunkHelperInternal::FLibPngEventHandler::user_malloc,
+		PngTextChunkHelperInternal::FLibPngEventHandler::user_free
+	);
+	png_infop info_ptr = png_create_info_struct(png_ptr);
+	png_bytep* row_pointers = (png_bytep*)png_malloc(png_ptr, Height * sizeof(png_bytep));
+	PngTextChunkHelperInternal::FPngReadGuard PNGGuard(&png_ptr, &info_ptr);
+	PNGGuard.SetRowPointers(row_pointers);
+
+	if (png_ptr == NULL || info_ptr == NULL)
+	{
+		return false;
+	}
+
+	if (setjmp(png_jmpbuf(png_ptr)) != 0)
+	{
+		return false;
+	}
+
+	const int32 ColorType = info_ptr->color_type;
+
+	if (ColorType == PNG_COLOR_TYPE_PALETTE)
+	{
+		png_set_palette_to_rgb(png_ptr);
+	}
+
+	if ((ColorType & PNG_COLOR_MASK_COLOR) == 0 && BitDepth < 8)
+	{
+		png_set_expand_gray_1_2_4_to_8(png_ptr);
+	}
+
+	// Insert alpha channel with full opacity for RGB images without alpha
+	if ((ColorType & PNG_COLOR_MASK_ALPHA) == 0 && (Format == ERGBFormat::BGRA || Format == ERGBFormat::RGBA))
+	{
+		// png images don't set PNG_COLOR_MASK_ALPHA if they have alpha from a tRNS chunk, but png_set_add_alpha seems to be safe regardless
+		if ((ColorType & PNG_COLOR_MASK_COLOR) == 0)
+		{
+			png_set_tRNS_to_alpha(png_ptr);
+		}
+		else if (ColorType == PNG_COLOR_TYPE_PALETTE)
+		{
+			png_set_tRNS_to_alpha(png_ptr);
+		}
+		if (BitDepth == 8)
+		{
+			png_set_add_alpha(png_ptr, 0xff, PNG_FILLER_AFTER);
+		}
+		else if (BitDepth == 16)
+		{
+			png_set_add_alpha(png_ptr, 0xffff, PNG_FILLER_AFTER);
+		}
+	}
+
+	// Calculate Pixel Depth
+	const uint64 PixelChannels = (Format == ERGBFormat::Gray) ? 1 : 4;
+	const uint64 BytesPerPixel = (BitDepth * PixelChannels) / 8;
+	const uint64 BytesPerRow = BytesPerPixel * Width;
+	RawData.Empty(Height * BytesPerRow);
+	RawData.AddUninitialized(Height * BytesPerRow);
+
+	png_set_read_fn(
+		png_ptr, 
+		ImageWrapper.Get(), 
+		PngTextChunkHelperInternal::FLibPngEventHandler::user_read_compressed
+	);
+
+	for (int64 i = 0; i < Height; i++)
+	{
+		row_pointers[i] = &RawData[i * BytesPerRow];
+	}
+	png_set_rows(png_ptr, info_ptr, row_pointers);
+
+	uint32 Transform = (Format == ERGBFormat::BGRA) ? PNG_TRANSFORM_BGR : PNG_TRANSFORM_IDENTITY;
+
+	// Convert grayscale png to RGB if requested
+	if ((ColorType & PNG_COLOR_MASK_COLOR) == 0 &&
+		(Format == ERGBFormat::RGBA || Format == ERGBFormat::BGRA))
+	{
+		Transform |= PNG_TRANSFORM_GRAY_TO_RGB;
+	}
+
+	// Convert RGB png to grayscale if requested
+	if ((ColorType & PNG_COLOR_MASK_COLOR) != 0 && Format == ERGBFormat::Gray)
+	{
+		png_set_rgb_to_gray_fixed(png_ptr, 2 /* warn if image is in color */, -1, -1);
+	}
+
+	// Strip alpha channel if requested output is grayscale
+	if (Format == ERGBFormat::Gray)
+	{
+		// this is not necessarily the best option, instead perhaps:
+		// png_color background = {0,0,0};
+		// png_set_background(png_ptr, &background, PNG_BACKGROUND_GAMMA_SCREEN, 0, 1.0);
+		Transform |= PNG_TRANSFORM_STRIP_ALPHA;
+	}
+
+	png_read_png(png_ptr, info_ptr, Transform, NULL);
 
 	png_textp TextPtr;
 	int32 NumText;
-	if (!png_get_text(ReadPtr, InfoPtr, &TextPtr, &NumText))
+	if (!png_get_text(png_ptr, info_ptr, &TextPtr, &NumText))
 	{
 		return false;
 	}
